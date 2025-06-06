b00e50c689ab3ee17cd30546e8fe9eace569853d
HatchedMarmot49 <llundell00@gmail.com>
2025-06-05 21:22:35 -0500

    table component skeleton

--- web-component/qr-web-component/src/app/app.component.ts
+++ web-component/qr-web-component/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WelcomeComponent } from "./components/welcome/welcome.component";
+import { TableComponent } from './components/table/table.component';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, WelcomeComponent],
+  imports: [RouterOutlet, WelcomeComponent, TableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
